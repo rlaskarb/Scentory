cb795a0aa595b1bcdbf8e339983130eca47399d3
rlaskarb <rlaskarb20@gmail.com>
2025-05-03 23:24:50 +0900

    시작

---
+++ Scentory/src/main/java/com/solo/scentory/ScentoryApplication.java
@@ -0,0 +1,13 @@
+package com.solo.scentory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScentoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScentoryApplication.class, args);
+    }
+
+}
---
+++ Scentory/src/test/java/com/solo/scentory/ScentoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solo.scentory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScentoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
